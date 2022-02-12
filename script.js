5c8142a1e1569fdc3e070f8cb7b879c9f532da7e
Shoyombo Gabriel Ayomide <74562680+DrPrime01@users.noreply.github.com>
2022-02-12 15:58:42 +0100

    Changed the quote file path

--- script.js
+++ script.js
@@ -60,9 +60,9 @@ const change = function () {
     quote.textContent = randomQuoteSelect;
     console.log(randomQuoteSelect);
 
-    let twitterAttr = `https://twitter.com/intent/tweet?text=+${randomQuoteSelect} \n ${author} \n \n Share! Let others find their favorite quote too \n https://drprime01.github.io/random-quote-generator/`
+    let twitterAttr = `https://twitter.com/intent/tweet?text=+${randomQuoteSelect} \n ${author} \n \n Share! Let others find their favorite quote too \n https://drprime01.github.io/Random-Quote-Generator/`
         twitter.setAttribute("href", twitterAttr)
-    let whatsappAttr = `whatsapp://send?text=+ ${randomQuoteSelect} \n ~ ${author} \n \n Share! Let others find their favorite quote too \n https://drprime01.github.io/random-quote-generator/`
+    let whatsappAttr = `whatsapp://send?text=+ ${randomQuoteSelect} \n ~ ${author} \n \n Share! Let others find their favorite quote too \n https://drprime01.github.io/Random-Quote-Generator/`
         whatsapp.setAttribute("href", whatsappAttr)
     
 }
